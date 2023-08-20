52d084204919d25381e9607fe036e0e762ae3d90
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-20 19:24:18 +0300

    ScrollView Important Props

--- App.js
+++ App.js
@@ -13,49 +13,119 @@ import image from "./assets/adaptive-icon.png";
 export default function App() {
   const [IsLoaded, setIsLoaded] = useState(false);
   const [IsError, setIsError] = useState(false);
+  const data = [
+    {
+      id: 1,
+      title: "The Importance of Reading",
+      description:
+        "Reading is essential for learning and personal growth. It helps us to expand our knowledge, develop our imagination, and connect with others. Reading can also be a great way to relax and de-stress.",
+    },
+    {
+      id: 2,
+      title: "The Power of Positivity",
+      description:
+        "Positive thinking can have a profound impact on our lives. It can help us to be more optimistic, resilient, and successful. When we focus on the positive, we are more likely to attract positive experiences into our lives.",
+    },
+    {
+      id: 3,
+      title: "The Benefits of Exercise",
+      description:
+        "Exercise is not only good for our physical health, but it is also good for our mental health. It can help to reduce stress, improve mood, and boost self-esteem. Exercise can also help us to sleep better and have more energy.",
+    },
+    {
+      id: 4,
+      title: "The Importance of Sleep",
+      description:
+        "Sleep is essential for our physical and mental health. It helps to repair our bodies, consolidate our memories, and regulate our emotions. When we do not get enough sleep, we can experience fatigue, irritability, and difficulty concentrating.",
+    },
+    {
+      id: 5,
+      title: "The Power of Gratitude",
+      description:
+        "Gratitude is the practice of appreciating the good things in our lives. It can help us to feel happier, more connected, and more fulfilled. When we express gratitude, we are more likely to focus on the positive and less likely to dwell on the negative.",
+    },
+    {
+      id: 6,
+      title: "The Importance of Self-Care",
+      description:
+        "Self-care is essential for our well-being. It is the practice of taking care of our physical, mental, and emotional health. Self-care can include activities such as getting enough sleep, eating healthy foods, exercising, and spending time with loved ones.",
+    },
+    {
+      id: 7,
+      title: "The Power of Forgiveness",
+      description:
+        "Forgiveness is the act of letting go of anger and resentment towards someone who has wronged us. It can be a difficult process, but it can be very healing. When we forgive, we are not saying that what happened was okay, but we are choosing to let go of the negative emotions that are holding us back.",
+    },
+    {
+      id: 8,
+      title: "The Importance of Love",
+      description:
+        "Love is one of the most powerful emotions we can experience. It can bring us joy, happiness, and fulfillment. Love can also help us to feel connected to others and to the world around us.",
+    },
+    {
+      id: 9,
+      title: "The Power of Hope",
+      description:
+        "Hope is the belief that things will get better. It can help us to persevere through difficult times and to achieve our goals. When we have hope, we are more likely to be optimistic and resilient.",
+    },
+  ];
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.top}>
-          <Text>Some Text</Text>
-          <Text style={styles.baseText}>
-            I am bold
-            <Text style={styles.innerText}> and red</Text>
-          </Text>
+    <ScrollView
+      contentContainerStyle={{
+        backgroundColor: "grey",
+        padding: "6",
+        margin: "6",
+      }}
+    >
+      {data.map((item) => (
+        <View style={{ margin: 16 }} key={item.id}>
+          <Text style={{ fontSize: 30 }}>{item.title}</Text>
+          <Text>{item.description}</Text>
         </View>
-        <View style={styles.middle}>
-          <TextInput
-            style={{
-              backgroundColor: "pink",
-              width: 300,
-              borderRadius: 20,
-              padding: 4,
-              fontSize: 20,
-            }}
-            placeholder="Type here..."
-            onFocus={()=>console.log("Focused")}
-            onBlur={()=>console.log("Blurred")}
-            placeholderTextColor={"grey"}
-            onChangeText={(val) => console.log(val)}
-          ></TextInput>
-        </View>
-        <View style={styles.bottom}>
-          <Image
-            onLoad={() => setIsLoaded(true)}
-            onError={() => setIsError(true)}
-            source={{
-              uri: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-            }}
-            style={{ width: 150, height: 150 }}
-          />
-          <Text>{IsLoaded && "Image loaded"}</Text>
-          <Text>{!IsError && "Image Error"}</Text>
-        </View>
-      </View>
-      <View pointerEvents="none">
-        <Button title="click Me" />
-      </View>
-    </>
+      ))}
+    </ScrollView>
+    // <>
+    //   <View style={styles.container}>
+    //     <View style={styles.top}>
+    //       <Text>Some Text</Text>
+    //       <Text style={styles.baseText}>
+    //         I am bold
+    //         <Text style={styles.innerText}> and red</Text>
+    //       </Text>
+    //     </View>
+    //     <View style={styles.middle}>
+    //       <TextInput
+    //         style={{
+    //           backgroundColor: "pink",
+    //           width: 300,
+    //           borderRadius: 20,
+    //           padding: 4,
+    //           fontSize: 20,
+    //         }}
+    //         placeholder="Type here..."
+    //         onFocus={()=>console.log("Focused")}
+    //         onBlur={()=>console.log("Blurred")}
+    //         placeholderTextColor={"grey"}
+    //         onChangeText={(val) => console.log(val)}
+    //       ></TextInput>
+    //     </View>
+    //     <View style={styles.bottom}>
+    //       <Image
+    //         onLoad={() => setIsLoaded(true)}
+    //         onError={() => setIsError(true)}
+    //         source={{
+    //           uri: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+    //         }}
+    //         style={{ width: 150, height: 150 }}
+    //       />
+    //       <Text>{IsLoaded && "Image loaded"}</Text>
+    //       <Text>{!IsError && "Image Error"}</Text>
+    //     </View>
+    //   </View>
+    //   <View pointerEvents="none">
+    //     <Button title="click Me" />
+    //   </View>
+    // </>
   );
 }
 
@@ -145,4 +215,17 @@ autoCorrect"true"
 onContentSizeChanges{(e)=>console.log(e.nativeEvent.content)}
 editable: "true" by default if false can focus and change in the textInput
 onSubmitEditing.
+*/
+/*
+ScrollView Important Props  <ScrollView></ScrollView>
+contentContainerStyle
+horizontal: to set the scroll to be horizontal
+showsVerticalScrollIndicator: shows vertical scrollbar"boolean"
+showsHorizontalScrollIndicator: shows horizantal scrollbar"boolean"
+scrollEnable by default is "true" "false" to disable the scrolling
+onScroll: function will be called when the user scrolls the view and the function
+will recieve an event object that contains info about scroll positions
+such as x,y position {(e)=>console.log(e.nativeEvent.contentOffest)}
+keyboardDismissMode: how keyboard dismiss works => 'on-drag'
+                            'none'/'interactive'/ 'on-drag'
 */
