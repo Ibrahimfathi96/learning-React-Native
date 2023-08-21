778affa2233e1920dac358b5bf96f8385d6e7adb
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-21 21:47:04 +0300

    DrawerLayoutAndroid & open & close & change its position

--- App.js
+++ App.js
@@ -1,25 +1,72 @@
-import { useEffect } from "react";
-import { BackHandler, View, Alert } from "react-native";
+import { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  DrawerLayoutAndroid,
+} from "react-native";
 export default function App() {
-  useEffect(() => {
-    const backAction = () => {
-      Alert.alert("Hold on!", "Are you sure you want to go back?", [
-        {
-          text: "Cancel",
-          onPress: () => null,
-          style: "cancel",
-        },
-        { text: "Exit", onPress: () => BackHandler.exitApp() },
-      ]);
-      return true;
-    };
-
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
-
-    return () => backHandler.remove();
-  }, []);
-  return <View></View>;
+  const [drawerPosition, setDrawerPosition] = useState("left");
+  const drawerRef = useRef(null);
+  return (
+    <DrawerLayoutAndroid
+      renderNavigationView={() => (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity
+            title="Close Drawer"
+            style={styles.button}
+            onPress={() => drawerRef.current.closeDrawer()}
+          >
+            <Text style={{ textAlign: "center" }}>Close Drawer</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      drawerPosition={drawerPosition}
+      drawerWidth={350}
+      ref={drawerRef}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity
+          title="Change Position"
+          style={styles.button}
+          onPress={() => {
+            drawerPosition === "left"
+              ? setDrawerPosition("right")
+              : setDrawerPosition("left");
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>Change Position</Text>
+        </TouchableOpacity>
+        <View style={{ height: 10, backgroundColor: "white" }}></View>
+        <TouchableOpacity
+          title="Open Drawer"
+          style={styles.button}
+          onPress={() => drawerRef.current.openDrawer()}
+        >
+          <Text style={{ textAlign: "center" }}>Open Drawer</Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerLayoutAndroid>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: 80,
+  },
+  button: {
+    padding: 12,
+    margin: 12,
+    justifyContent: "center",
+    alignContent: "center",
+    textAlign: "center",
+    backgroundColor: "grey",
+    width: 150,
+    borderRadius: 20,
+  },
+});
