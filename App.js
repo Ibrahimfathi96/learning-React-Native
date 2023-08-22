73db7e8230e7bf48734316cf6d860ae3b70b70cb
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-23 02:06:30 +0300

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,78 +1,8 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  PermissionsAndroid,
-  Button,
-  ToastAndroid,
-} from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 export default function App() {
-  const showToast = () => {
-    ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.SHORT);
-  };
-
-  const showToastWithGravity = () => {
-    ToastAndroid.showWithGravity(
-      "All Your Base Are Belong To Us",
-      ToastAndroid.SHORT,
-      ToastAndroid.CENTER
-    );
-  };
-
-  const showToastWithGravityAndOffset = () => {
-    ToastAndroid.showWithGravityAndOffset(
-      "A wild toast appeared!",
-      ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
-      25,
-      50
-    );
-  };
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "Cool Photo App Camera Permission",
-          message:
-            "Cool Photo App needs access to your camera " +
-            "so you can take awesome pictures.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK",
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the camera");
-      } else {
-        console.log("Camera permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        title="request permissions"
-        style={styles.button}
-        onPress={requestCameraPermission}
-      >
-        <Text style={{ textAlign: "center" }}>Try permissions</Text>
-      </TouchableOpacity>
-      <View style={{ height: 20 }}></View>
-      <Button title="Toggle Toast" onPress={() => showToast()} />
-      <View style={{ height: 20 }}></View>
-      <Button
-        title="Toggle Toast With Gravity"
-        onPress={() => showToastWithGravity()}
-      />
-      <View style={{ height: 20 }}></View>
-      <Button
-        title="Toggle Toast With Gravity & Offset"
-        onPress={() => showToastWithGravityAndOffset()}
-      />
+      <ActivityIndicator animating={true} size={30} color={"black"} />
     </View>
   );
 }
@@ -84,6 +14,7 @@ const styles = StyleSheet.create({
     marginTop: 80,
   },
   button: {
+    flex: 1,
     padding: 12,
     margin: 12,
     justifyContent: "center",
