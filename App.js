26e295914ebd81fc2cddd2b8efa5e7315b1335a0
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-23 15:50:00 +0300

    Linking

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-import { View, StyleSheet, useWindowDimensions } from "react-native";
+import { View, Button, Linking } from "react-native";
 
-const useStyles = () => {
-  const { height, width, scale, fonstScale } = useWindowDimensions();
-
-  return StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      flex: 1,
-    },
-    left: {
-      backgroundColor: "pink",
-      width: width / 2,
-    },
-    right: {
-      backgroundColor: "skyblue",
-      flex: 1,
-    },
-  });
-};
 const App = () => {
-  const styles = useStyles();
   return (
-    <View style={styles.container}>
-      <View style={styles.left} />
-      <View style={styles.right} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        title="Send mail"
+        onPress={() => Linking.openURL("mailto: support@expo.io")}
+      />
+      <Button
+        title="Open Phone"
+        onPress={() => Linking.openURL("tel:+123456789")}
+      />
+      <Button
+        title="Open sms"
+        onPress={() => Linking.openURL("sms:+123456789")}
+      />
+      <Button
+        title="Open url"
+        onPress={() => Linking.openURL("https://expo.io")}
+      />
     </View>
   );
 };
