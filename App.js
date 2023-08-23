06a17700a45dd56a0dacfe194542c1cc7df3debc
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-23 03:44:44 +0300

    dimensions

--- App.js
+++ App.js
@@ -1,78 +1,31 @@
-import React, { useEffect, useRef } from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Animated,
-} from "react-native";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 
+const useStyles = () => {
+  const { height, width, scale, fonstScale } = useWindowDimensions();
+
+  return StyleSheet.create({
+    container: {
+      flexDirection: "row",
+      flex: 1,
+    },
+    left: {
+      backgroundColor: "pink",
+      width: width / 4,
+    },
+    right: {
+      backgroundColor: "skyblue",
+      flex: 1,
+    },
+  });
+};
 const App = () => {
-  const spinAnimated = useRef(new Animated.Value(0)).current;
-  const startAnimation = () => {
-    Animated.loop(
-      Animated.timing(spinAnimated, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      })
-    ).start();
-  };
+  const styles = useStyles();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={startAnimation}>
-        <Text style={styles.buttonRow}>Start Animation</Text>
-        <Animated.View
-          style={[
-            styles.box,
-            {
-              transform: [
-                {
-                  rotate: spinAnimated.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: ["0deg", "360deg"],
-                  }),
-                },
-              ],
-            },
-          ]}
-        ></Animated.View>
-      </TouchableOpacity>
+      <View style={styles.left} />
+      <View style={styles.right} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  box: {
-    width: 100,
-    height: 100,
-    margin: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
-  },
-  fadingContainer: {
-    padding: 20,
-    backgroundColor: "powderblue",
-  },
-  fadingText: {
-    fontSize: 28,
-  },
-  buttonRow: {
-    justifyContent: "center",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    padding: 16,
-    fontSize: 18,
-    borderRadius: 26,
-    color: "white",
-    backgroundColor: "blue",
-  },
-});
 
 export default App;
